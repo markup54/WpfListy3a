6d60bc286e06b8a7bf7e2b4257badc734d7a28ca
markup54 <marzenakupka@tlen.pl>
2023-12-05 12:03:27 +0100

    itemsControl

--- WpfListy3a/MainWindow.xaml.cs
+++ WpfListy3a/MainWindow.xaml.cs
@@ -20,9 +20,18 @@ namespace WpfListy3a
     /// </summary>
     public partial class MainWindow : Window
     {
+        public List<String> Teksty { get; set; } 
         public MainWindow()
         {
             InitializeComponent();
+            Teksty = new List<String>();
+            Teksty.Add("pierwszy zapis");
+            Teksty.Add("drugi zapis");
+            Teksty.Add("cokolwiek");
+            lista3.ItemsSource = Teksty;
+            DataContext = this;
+
         }
+
     }
 }
